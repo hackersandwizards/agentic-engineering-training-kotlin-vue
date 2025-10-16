74841d0a1ae1fdf52c079a2ab576be82720bbd27
Benedikt Stemmildt <benedikt@stemmildt.com>
2025-10-16 09:57:40 +0200

    Revert "feat: implement Beschreibung value object with factory pattern (task-1)"
    This reverts commit 908e938355c2c2c3e4a85b76ab9c06de3f2cca59.

--- src/main/kotlin/de/blume2000/finden/domain/model/produkte/produkt/BeschreibungIstLeerException.kt
+++
@@ -1,5 +0,0 @@
-package de.blume2000.finden.domain.model.produkte.produkt
-
-import de.blume2000.finden.domain.model.produkte.ProdukteException
-
-class BeschreibungIstLeerException(fehlermeldung: String) : ProdukteException(fehlermeldung)
